a581828e8081e0cd239092a88c8bd0e72fb85caa
Mariyan Apostolov <mariyan_apostolov@mail.bg>
2018-02-07 19:25:26 +0200

    Lists Exercises - C# Part 2

---
+++ Tech_Module/Programming_Fundamentals/06_Lists_Exercises/C#/Lists_Exercises/04_Longest_Increasing_Subsequence/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04_Longest_Increasing_Subsequence
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] sequence = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[] longestSeq = FindLongestIncreasingSubsequence(sequence);
+            Console.WriteLine(string.Join(" ", longestSeq));
+        }
+
+        public static int[] FindLongestIncreasingSubsequence(int[] sequence)
+        {
+            int[] length = new int[sequence.Length];
+            int[] prev = new int[sequence.Length];
+            int maxLength = 0;
+            int lastIndex = -1;
+
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                length[i] = 1;
+                prev[i] = -1;
+
+                for (int j = 0; j < i; j++)
+                {
+                    if (sequence[j] < sequence[i] && length[j] >= length[i])
+                    {
+                        length[i] = 1 + length[j];
+                        prev[i] = j;
+                    }
+                }
+
+                if (length[i] > maxLength)
+                {
+                    maxLength = length[i];
+                    lastIndex = i;
+                }
+            }
+
+            List<int> longestSeq = new List<int>();
+            for (int i = 0; i < maxLength; i++)
+            {
+                longestSeq.Add(sequence[lastIndex]);
+                lastIndex = prev[lastIndex];
+            }
+
+            longestSeq.Reverse();
+            return longestSeq.ToArray();
+        }
+    }
+}
---
+++ Tech_Module/Programming_Fundamentals/06_Lists_Exercises/C#/Lists_Exercises/05_Array_Manipulator/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05_Array_Manipulator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            string line = string.Empty;
+            while (!(line = Console.ReadLine()).Equals("print"))
+            {
+                string[] lineArray = line.Split(' ');
+                if (lineArray[0].Equals("add"))
+                {
+                    list.Insert(int.Parse(lineArray[1]), int.Parse(lineArray[2]));
+                }
+                else if (lineArray[0].Equals("addMany"))
+                {
+                    var collection = lineArray
+                        .Skip(2)
+                        .Select(int.Parse)
+                        .ToList();
+                    list.InsertRange(int.Parse(lineArray[1]), collection);
+                }
+                else if (lineArray[0].Equals("contains"))
+                {
+                    Console.WriteLine(list.IndexOf(int.Parse(lineArray[1])));
+                }
+                else if (lineArray[0].Equals("remove"))
+                {
+                    list.RemoveAt(int.Parse(lineArray[1]));
+                }
+                else if (lineArray[0].Equals("shift"))
+                {
+                    var rotated = new List<int>();
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        rotated.Add(list[(i + int.Parse(lineArray[1])) % list.Count]);
+                    }
+                    list.Clear();
+                    list.AddRange(rotated);
+                }
+                else if (lineArray[0].Equals("sumPairs"))
+                {
+                    var sumList = new List<int>();
+                    for (int i = 0; i < list.Count - 1; i += 2)
+                    {
+                        sumList.Add(list[i] + list[i + 1]);
+                    }
+                    if (list.Count % 2 != 0)
+                    {
+                        sumList.Add(list[list.Count - 1]);
+                    }
+                    list.Clear();
+                    list.AddRange(sumList);
+                }
+            }
+            Console.WriteLine("[{0}]", string.Join(", ", list));
+        }
+    }
+}
