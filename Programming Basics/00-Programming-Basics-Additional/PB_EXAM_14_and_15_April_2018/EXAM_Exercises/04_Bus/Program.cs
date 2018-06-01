8f84ff212532c9cc95bf29152468a96699e41c0a
Mariyan Apostolov <mariyan_apostolov@mail.bg>
2018-06-01 23:42:12 +0300

    Programming Basics EXAM 14 and 15 April 2018

---
+++ Programming Basics/00-Programming-Basics-Additional/PB_EXAM_14_and_15_April_2018/EXAM_Exercises/04_Bus/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _04_Bus
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int travellersCount = int.Parse(Console.ReadLine());
+            int busStopsCount = int.Parse(Console.ReadLine());
+            
+            for (int i = 1; i <= busStopsCount; i += 1)
+            {
+                int passengersOutCount = int.Parse(Console.ReadLine());
+                int passengersInCount = int.Parse(Console.ReadLine());
+                travellersCount -= passengersOutCount;
+                travellersCount += passengersInCount;
+
+                if (i % 2 == 0)
+                {
+                    travellersCount -= 2;
+                }
+                else
+                {
+                    travellersCount += 2;
+                }
+            }
+            Console.WriteLine($"The final number of passengers is : {travellersCount}");
+        }
+    }
+}
---
+++ Programming Basics/00-Programming-Basics-Additional/PB_EXAM_14_and_15_April_2018/EXAM_Exercises/06_Car_Number/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _06_Car_Number
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+
+            for (int p1 = start; p1 <= end; p1++)
+            {
+                for (int p2 = start; p2 <= end; p2++)
+                {
+                    for (int p3 = start; p3 <= end; p3++)
+                    {
+                        for (int p4 = start; p4 <= end; p4++)
+                        {
+                            if ((p1 % 2 == 0 && p4 % 2 != 0) || (p1 % 2 != 0 && p4 % 2 == 0))
+                            {
+                                if (p1 > p4)
+                                {
+                                    if ((p2 + p3) % 2 == 0)
+                                    {
+                                        Console.Write($"{p1}{p2}{p3}{p4} ");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
